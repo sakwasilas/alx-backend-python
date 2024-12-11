9156249f064cb2d82d5924ee975bec6e890736db
sakwasilas <sakwahsilas@gmail.com>
2024-12-11 02:33:40 -0800

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,16 +19,67 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that access_nested_map returns the expected result."""
         self.assertEqual(access_nested_map(nested_map, path), expected
-      @parameterized.expand([
+       @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """
-        Test that access_nested_map raises a KeyError for invalid paths
-        and that the exception message is correct.
+         test that a KeyError is raised for
+         the following inputs (use @parameterized.expand)
         """
-        with self.assertRaises(KeyError) as context:
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """
+     implement the TestGetJson.test_get_json
+     method to test that utils.get_json returns the expected result.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """
+        method to test that the method returns what it is supposed to.
+        """
+        class Mocked(Mock):
+            """
+            class that inherits from Mock
+            """
+
+            def json(self):
+                """
+                json returning a payload
+                """
+                return payload
+
+        with patch("requests.get") as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(unittest.TestCase):
+    ''' memoize unittest '''
+
+    def test_memoize(self):
+        ''' memoize test '''
+
+        class TestClass:
+            ''' self descriptive'''
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
 
