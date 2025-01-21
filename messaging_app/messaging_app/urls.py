852a8398003cf48c996a9ffb88a09d3089df4e71
silassakwa <you@example.comsakwahsilas@gmail.comgit config --global user.email you@example.comsakwahsilas@gmail.com>
2025-01-21 17:57:02 +0300

    Added serializers for User, Message, and Conversation with nested relationships

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Keep the admin path
+    path('api/', include('chats.urls')),  # Add this line to include the API routes under /api/
 ]
