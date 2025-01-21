3061f0417b280454e7996889e57b773845880222
silassakwa <you@example.comsakwahsilas@gmail.comgit config --global user.email you@example.comsakwahsilas@gmail.com>
2025-01-21 17:53:02 +0300

    Implemented API endpoints for conversations and messages

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter  # Make sure this import is included
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register the viewsets with the router
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),  # Include the generated URLs from the router
+]
