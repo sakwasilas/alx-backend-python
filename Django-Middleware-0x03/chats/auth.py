79af18fbdbc7dd5e64ae1e4e12d4ccf265f79877
silassakwa <you@example.comsakwahsilas@gmail.comgit config --global user.email you@example.comsakwahsilas@gmail.com>
2025-01-29 15:40:54 +0300

    alx backend python

--- Django-Middleware-0x03/chats/auth.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    Custom JWT authentication class to allow for custom authentication.
-    """
-    pass
