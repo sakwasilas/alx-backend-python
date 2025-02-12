ddfb4982605e7bb465a656bf90ac14a6a0320c23
silassakwa <sakwahsilas@gmail.com>
2025-02-12 17:50:02 +0300

    added serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,17 +3,28 @@ from .models import User, Conversation, Message
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['user_id', 'username', 'email', 'phone_number', 'role']
+        fields = ['user_id', 'username', 'email', 'phone_number', 'role', 'full_name']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 # Message Serializer
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField(max_length=1000)
 
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
+
+    def validate_message_body(self, value):
+        if len(value.strip()) == 0:
+            raise serializers.ValidationError("Message body cannot be empty.")
+        return value
 
 # Conversation Serializer
 class ConversationSerializer(serializers.ModelSerializer):
